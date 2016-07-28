f9f97103fd04e80a7b217571c4e4961f82e40f8a
Viktor Stanchev <me@viktorstanchev.com>
2016-07-28 07:42:33 -0700

    build bitcoind and litecoind, clean up

---
+++ gen_secrets.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#openssl genrsa -out key.pem 2048
+#openssl req -new -x509 -nodes -sha256 -days 3650 -key key.pem > cert.pem
+
+mkdir -p secrets/bitcoind
+pushd secrets/bitcoind
+dd if=/dev/urandom bs=32 count=1 of=spiped_secret
+popd
+
+mkdir -p secrets/litecoind
+pushd secrets/litecoind
+dd if=/dev/urandom bs=32 count=1 of=spiped_secret
+popd
